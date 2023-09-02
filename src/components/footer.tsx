5c6fcd54955b549a6c98d55cfcb82f758fe84717
SteveSande <stevenandrewsande@gmail.com>
2023-09-02 14:30:34 -0400

    page structure

---
+++ src/components/footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="bg-red-100 p-[50px] mt-auto flex justify-evenly">
+      <h1>block</h1>
+      <h1>block</h1>
+      <h1>block</h1>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-blue-100 flex justify-around p-[50px]">
+      <h1>Work Experience</h1>
+      <h1>Projects</h1>
+      <h1>About Me</h1>
+    </header>
+  );
+}
+
+export default Header;
