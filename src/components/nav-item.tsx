b4f7612a040f846e3a1905afb224a765b56d3d5c
ssande2458 <ssande2458@conestogac.on.ca>
2023-09-04 15:22:24 -0400

    more css, added name as title

--- src/components/nav-item.tsx
+++ src/components/nav-item.tsx
@@ -7,9 +7,9 @@ interface info {
 
 function NavItem(props: info) {
   return (
-    <NavigationMenu.Item>
+    <NavigationMenu.Item className="flex justify-center border-solid border-black border-2 w-1/3">
         <NavigationMenu.Link
-          className="border-solid border-black border-2"
+          className='p-2 border-solid border-green-300 border-2 text-xl'
           href={props.link}
         >
           {props.text}
